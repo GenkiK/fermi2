df6dfc1120fc9c38e7b186546a938cf2bf962505
hongu-ku <hongu.sol.0901@gmail.com>
2021-03-29 11:24:20 +0900

    不要な構造体を削除

--- 20210329/fermi.cpp
+++ 20210329/fermi.cpp
@@ -10,57 +10,6 @@
 #define FORR(i, a, b) for (int i = (int)(b)-1; i >= a; i--)
 #define ALL(a) a.begin(), a.end()
 using namespace std;
-int si()
-{
-  int x;
-  scanf("%d", &x);
-  return x;
-}
-long long sl()
-{
-  long long x;
-  scanf("%lld", &x);
-  return x;
-}
-string ss()
-{
-  string x;
-  cin >> x;
-  return x;
-}
-void pi(int x) { printf("%d ", x); }
-void pl(long long x) { printf("%lld ", x); }
-void pd(double x) { printf("%.15f ", x); }
-void ps(const string &s) { printf("%s ", s.c_str()); }
-void br() { putchar('\n'); }
-
-const ll MOD = 1e9 + 7;
-const ll INF = 1e9 + 5;
-
-struct mint
-{
-  int n;
-  mint(int n_ = 0) : n(n_) {}
-};
-
-mint operator+(mint a, mint b)
-{
-  a.n += b.n;
-  if (a.n >= MOD)
-    a.n -= MOD;
-  return a;
-}
-mint operator-(mint a, mint b)
-{
-  a.n -= b.n;
-  if (a.n < 0)
-    a.n += MOD;
-  return a;
-}
-mint operator*(mint a, mint b) { return (long long)a.n * b.n % MOD; }
-mint &operator+=(mint &a, mint b) { return a = a + b; }
-mint &operator-=(mint &a, mint b) { return a = a - b; }
-mint &operator*=(mint &a, mint b) { return a = a * b; }
 
 typedef pair<int, int> P;
 
