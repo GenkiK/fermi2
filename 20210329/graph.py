4a0f94a57c8cbf48df5c2b1cba973f61bb9d5241
hongu-ku <hongu.sol.0901@gmail.com>
2021-03-31 14:57:37 +0900

    c++の実行条件を変更

--- 20210329/graph.py
+++ 20210329/graph.py
@@ -5,13 +5,21 @@ import numpy as np
 import csv
 import pandas as pd
 import subprocess
+import os
 
 # %%
-# c++実行
-n = 3  # 電子の数[個]
-lim_size = 100  # 最大の総エネルギー[ε]の個数 [(n-1)*n / 2,  (n-1)*n / 2 + lim_size]
-cmd = "./a.out " + str(n) + " " + str(lim_size)
-subprocess.run(cmd.split())
+# ファイルが存在しないorデータが足りない場合,c++実行
+n = 8  # 電子の数[個]
+lim_size = 5  # 最大の総エネルギー[ε]の個数 [(n-1)*n / 2,  (n-1)*n / 2 + lim_size]
+
+filename = "./state_count" + str(n) + ".csv"
+if os.path.exists(filename):
+    line_count = (
+        int(subprocess.check_output(["wc", "-l", filename]).decode().split(" ")[0]) - 1
+    )
+    if line_count < lim_size:
+        cmd = "./a.out " + str(n) + " " + str(lim_size)
+        subprocess.run(cmd.split())
 
 # %%
 filename = "./state_count" + str(n) + ".csv"
