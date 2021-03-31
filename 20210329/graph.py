9fedd0c4b5a5b785807718a8e00cc93320ee71bb
hongu-ku <hongu.sol.0901@gmail.com>
2021-03-31 14:51:09 +0900

    コメントを修正

--- 20210329/graph.py
+++ 20210329/graph.py
@@ -9,7 +9,7 @@ import subprocess
 # %%
 # c++実行
 n = 3  # 電子の数[個]
-lim_size = 100  # 最大の総エネルギー[ε]の個数 [(n-1)!    (n-1)! + lim_size]
+lim_size = 100  # 最大の総エネルギー[ε]の個数 [(n-1)*n / 2,  (n-1)*n / 2 + lim_size]
 cmd = "./a.out " + str(n) + " " + str(lim_size)
 subprocess.run(cmd.split())
 
